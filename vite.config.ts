89b404e697f8dcd2f8471aecacd06f615bc0781a
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-08-05 21:31:37 +0900

    [docs]: add svgr setting (#10)

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+import svgr from "vite-plugin-svgr";
 import Fonts from "unplugin-fonts/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    svgr(),
     Fonts({
       google: {
         families: [
