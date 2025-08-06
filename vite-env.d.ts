6052f2d54b6c4c746483737c8c76337fb5efa45a
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-08-06 16:27:35 +0900

    [docs]: move vite env file location (#10)

--- vite-env.d.ts
+++
@@ -1,7 +0,0 @@
-﻿// <reference types="vite/client" />
-
-declare module "*.svg?react" {
-  import * as React from "react";
-  const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
-  export default ReactComponent;
-}
