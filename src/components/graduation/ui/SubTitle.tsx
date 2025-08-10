572d3974cb645d552e0512b62086d4d2cc5220f5
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-08-11 02:48:41 +0900

    [ui]: add SubTitle component (#9)

--- src/components/graduation/ui/SubTitle.tsx
+++ src/components/graduation/ui/SubTitle.tsx
@@ -1,1 +1,18 @@
-﻿
+﻿import React from "react";
+
+interface SubtitleProps {
+  title: string;
+}
+
+const Subtitle: React.FC<SubtitleProps> = ({ title }) => {
+  const subtitleClasses = `
+    self-stretch text-darkgray text-base md:text-xl leading-[48px]
+    md:pb-2
+  `;
+
+  return (
+    <div className={subtitleClasses.trim().replace(/\s+/g, " ")}>{title}</div>
+  );
+};
+
+export default Subtitle;
