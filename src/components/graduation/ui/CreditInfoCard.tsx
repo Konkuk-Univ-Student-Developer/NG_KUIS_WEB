0fe936e18057a1fe13b20353246effaa2aba0112
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-08-10 22:51:15 +0900

    [ui]: add graudation ui components (#9)

---
+++ src/components/graduation/ui/CreditInfoCard.tsx
@@ -0,0 +1,25 @@
+﻿interface CreditInfoCardProps {
+  title: string;
+  value: number | string;
+  unit: string;
+}
+
+const CreditInfoCard = ({ title, value, unit }: CreditInfoCardProps) => {
+  return (
+    <div className="flex flex-col">
+      <span className="text-black text-base md:text-xl font-normal">
+        {title}
+      </span>
+      <div className="flex items-baseline mt-1">
+        <span className="text-darkgreen text-lg md:text-xl font-bold">
+          {value}
+        </span>
+        <span className="text-darkgray text-lg md:text-xl font-normal ml-1">
+          {unit}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default CreditInfoCard;
