4cd49b6735afcd3ed05c8a4aaeb64959846f45ae
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-08-11 01:40:43 +0900

    [ui]: add GraudationStatus component (#9)

---
+++ src/components/graduation/ui/GraduationStatus.tsx
@@ -0,0 +1,48 @@
+﻿import React from "react";
+
+interface StatusCircleProps {
+  status: "pass" | "non-pass";
+  canGraduateEarly: boolean;
+}
+
+const StatusCircle: React.FC<StatusCircleProps> = ({
+  status,
+  canGraduateEarly,
+}) => {
+  const isPass = status === "pass";
+  const circleBgColor = isPass ? "bg-blue" : "bg-danger";
+  const displayText = isPass ? "PASS" : "FAIL";
+
+  const subText = `조기졸업: ${canGraduateEarly ? "Y" : "N"}`;
+
+  return (
+    <div className="inline-flex flex-col items-center justify-center gap-2 md:gap-3">
+      <div
+        className={`
+          flex flex-col items-center justify-center rounded-full
+          h-28 w-28 p-6
+          md:h-[148px] md:w-[148px] md:px-[46px] md:py-[30px]
+          ${circleBgColor} 
+        `}
+      >
+        <div
+          className="text-center font-bold text-white
+            text-2xl leading-tight
+            md:text-[28px] md:leading-[56px]"
+        >
+          {displayText}
+        </div>
+      </div>
+
+      <div
+        className="text-center text-darkgray
+        text-sm leading-normal 
+        md:text-base md:leading-loose"
+      >
+        {subText}
+      </div>
+    </div>
+  );
+};
+
+export default StatusCircle;
