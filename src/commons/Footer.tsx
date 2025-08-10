16ae50076945ab84c41a784581dfe45c3ff1d3d5
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-08-10 17:53:46 +0900

    [refactor]: seperate footer constants (#8)

--- src/commons/Footer.tsx
+++ src/commons/Footer.tsx
@@ -1,52 +1,54 @@
-﻿import Instagram from "@/assets/icon/ic_instagram.svg?react";
-import Github from "@/assets/icon/ic_github.svg?react";
-import Youtube from "@/assets/icon/ic_youtube.svg?react";
+﻿import {Link} from "react-router-dom"; 
+import { SOCIAL_LINKS } from "@/constants/FooterConstants";
+
+type SocialLinkProps = {
+  path: string;
+  Icon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
+};
+
+function SocialLink({ path, Icon }: SocialLinkProps) {
+  return (
+    <Link to={path} target="_blank" rel="noopener noreferrer">
+      <Icon className="w-6 h-6 md:w-8 md:h-8" />
+    </Link>
+  );
+}
 
 function Footer() {
   return (
-    <footer className="hidden md:flex w-full h-full px-6 md:px-24 py-8 bg-white border-t-[0.3px] border-darkgray flex-row justify-between items-center md:items-start text-darkgray">
-      <div className="flex h-full flex-col py-3 justify-between items-start md:items-start">
-        <div>
-          <div className="text-sm font-semibold">건국대학교</div>
-          <div className="text-xs font-extralight mb-4">
-            (05029) 120 Neungdong-ro, Gwangjin-gu, Seoul 05029. KOREA
-            <br />
-            TEL 02-450-3114
+    <footer className="hidden md:flex w-full py-8 bg-white border-t-[0.3px] border-darkgray text-darkgray">
+      <div className="w-full max-w-7xl mx-auto px-6 flex flex-row justify-between items-center md:items-start">
+        <div className="flex h-full flex-col py-3 justify-between items-start md:items-start">
+          <div>
+            <div className="text-sm font-semibold">건국대학교</div>
+            <div className="text-xs font-extralight mb-4">
+              (05029) 120 Neungdong-ro, Gwangjin-gu, Seoul 05029. KOREA
+              <br />
+              TEL 02-450-3114
+            </div>
+          </div>
+          <div>
+            <div className="text-xs font-light md:text-left">
+              COPYRIGHT © 2025 KONKUK UNIVERSITY STUDENTS DEVELOPER
+              <br />
+              ALL RIGHTS RESERVED
+            </div>
           </div>
         </div>
-        <div>
-          <div className="text-xs font-light md:text-left">
-            COPYRIGHT © 2025 KONKUK UNIVERSITY STUDENTS DEVELOPER
-            <br />
-            ALL RIGHTS RESERVED
-          </div>
+
+        <div className="flex min-h-full self-stretch justify-center md:justify-start items-center gap-6">
+          {SOCIAL_LINKS.map((link) => (
+            <SocialLink
+              key={link.id}
+              path={link.path}
+              Icon={link.icon}
+            />
+          ))}
         </div>
-      </div>
-      <div className="flex min-h-full self-stretch justify-center md:justify-start items-center gap-6">
-        <a
-          href="https://github.com/Konkuk-Univ-Student-Developer/NG_KUIS_WEB"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Github className="w-6 h-6 md:w-8 md:h-8" />
-        </a>
-        <a
-          href="https://www.youtube.com/@KonkukUniv"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Youtube className="w-6 h-6 md:w-8 md:h-8" />
-        </a>
-        <a
-          href="https://www.instagram.com/konkuk_official"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Instagram className="w-6 h-6 md:w-8 md:h-8" />
-        </a>
       </div>
     </footer>
   );
 }
 
 export default Footer;
+
---
+++ src/constants/FooterConstants.tsx
@@ -0,0 +1,21 @@
+﻿import Github from "@/assets/icon/ic_github.svg?react";
+import Youtube from "@/assets/icon/ic_youtube.svg?react";
+import Instagram from "@/assets/icon/ic_instagram.svg?react";
+
+export const SOCIAL_LINKS = [
+  {
+    id: "github",
+    path: "https://github.com/Konkuk-Univ-Student-Developer/NG_KUIS_WEB",
+    icon: Github,
+  },
+  {
+    id: "youtube",
+    path: "https://www.youtube.com/@KonkukUniv",
+    icon: Youtube,
+  },
+  {
+    id: "instagram",
+    path: "https://www.instagram.com/konkuk_official",
+    icon: Instagram,
+  },
+];
