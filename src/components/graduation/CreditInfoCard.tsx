1304a32679a4483521cc17d42c845c9c99c5a85f
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-08-14 18:22:54 +0900

    [ui]: change infoCard font size (#9)

--- src/components/graduation/CreditInfoCard.tsx
+++ src/components/graduation/CreditInfoCard.tsx
@@ -11,10 +11,10 @@ const CreditInfoCard = ({ title, value, unit }: CreditInfoCardProps) => {
         {title}
       </span>
       <div className="flex items-baseline mt-1">
-        <span className="text-darkgreen text-base md:text-3xl font-bold">
+        <span className="text-darkgreen text-lg md:text-3xl font-bold">
           {value}
         </span>
-        <span className="text-darkgray text-sm md:text-2xl font-normal ml-1">
+        <span className="text-darkgray text-base md:text-2xl font-normal ml-1">
           {unit}
         </span>
       </div>
--- src/components/graduation/GraduationTable.tsx
+++ src/components/graduation/GraduationTable.tsx
@@ -1,4 +1,5 @@
-﻿import type { TableProps } from "@/types/graduation";
+﻿import { cellBaseClasses, headerTextClasses, valueTextClasses } from "@/constants/GraduationConstants";
+import type { TableProps } from "@/types/graduation";
 import React from "react";
 
 const GraduationTable: React.FC<TableProps> = ({
@@ -6,11 +7,6 @@ const GraduationTable: React.FC<TableProps> = ({
   rows,
   headerBgColor = "bg-beige",
 }) => {
-  const cellBaseClasses =
-    "flex justify-center items-center p-2 flex-shrink-0 break-keep";
-  const headerTextClasses =
-    "text-black text-sm md:text-lg font-bold text-center";
-  const valueTextClasses = "text-black text-sm md:text-lg text-center";
 
   return (
     <div className="flex flex-col rounded overflow-hidden border border-coolgray">
