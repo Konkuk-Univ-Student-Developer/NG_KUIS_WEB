7f200d6bccb1c32f9e0f1093435141865d9dbbe8
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2025-08-16 01:35:59 +0900

    [ui]: 공지사항 날짜 ui 수정 (#22)

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,21 +1,14 @@
 type DateFormatVariant = "full" | "short";
 
-// 날짜 형식을 'YYYY-MM-DD'에서 'MM.DD' 또는 'YYYY-MM-DD'로 변환하는 함수
 export const formatDate = (
   dateString: string,
   variant: DateFormatVariant = "full"
 ): string => {
-  const parts = dateString.split("-");
-  if (parts.length < 3) {
-    console.error("Invalid date format. Expected 'YYYY-MM-DD'.");
-    return dateString;
-  }
-
-  const [, month, day] = parts;
+  const datePart = dateString.split(" ")[0].replace(/-/g, ".");
 
   if (variant === "short") {
-    return `${month}.${day}`;
+    return datePart.slice(5);
   }
 
-  return dateString.replace(/-/g, ".");
+  return datePart;
 };
