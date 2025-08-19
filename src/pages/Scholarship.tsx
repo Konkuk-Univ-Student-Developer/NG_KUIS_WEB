22c408aecd1839b2d6d2c6a1af2d021223d46174
jinny3824 <jinny3824@naver.com>
2025-08-19 18:25:52 +0900

    [refactor]: 장학관리 페이지 경로 수정 및 파일 세팅 (#19)

--- src/pages/Scholarship.tsx
+++ src/pages/Scholarship.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 const ScholarshipPage: React.FC = () => {
   return (
-    <div className="min-h-screen bg-white md:bg-gray-50">
+    <div className="flex flex-col gap-6 md:mx-auto md:max-w-350 py-8 md:block md:px-16 lg:px-24 md:py-12">
+      <h2 className="text-xl font-bold leading-[1.4] text-darkgreen ml-5 md:m-0 md:pb-18 md:text-center md:text-4xl md:font-bold md:leading-[2.0]">
+        장학 관리
+      </h2>
     </div>
   );
 };
