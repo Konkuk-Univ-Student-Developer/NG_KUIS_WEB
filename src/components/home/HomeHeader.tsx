bb976b6f77d6126adaeca05ca62a654024804d99
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2025-09-03 11:18:37 +0900

    [fix]: 홈화면 로그인 버튼 클릭하면 바로 로그인되도록 수정

--- src/components/home/HomeHeader.tsx
+++ src/components/home/HomeHeader.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import useAuthStore from "@/stores/authStore";
 
 interface HomeHeaderProps {
   isLoggedIn: boolean;
@@ -6,6 +6,8 @@ interface HomeHeaderProps {
 }
 
 const HomeHeader = ({ isLoggedIn, userName }: HomeHeaderProps) => {
+  const { login } = useAuthStore();
+
   return (
     <>
       <h2 className="text-black">
@@ -24,12 +26,12 @@ const HomeHeader = ({ isLoggedIn, userName }: HomeHeaderProps) => {
         ) : (
           <div className="flex flex-col md:flex-row md:gap-2 md:justify-center">
             <div className="inline-block">
-              <Link
-                to="/login"
-                className="text-darkgreen text-lg font-bold leading-[1.4] tracking-[-0.36px] border-b-2 border-darkgreen md:text-4xl md:font-bold md:leading-[2.0] md:tracking-[-0.72]"
+              <button
+                onClick={login}
+                className="text-darkgreen text-lg font-bold leading-[1.4] tracking-[-0.36px] border-b-2 border-darkgreen md:text-4xl md:font-bold md:tracking-[-0.72]"
               >
                 로그인
-              </Link>
+              </button>
               <span className="text-lg font-normal leading-[1.4] tracking-[-0.36px] md:text-4xl md:font-bold md:leading-[2.0] md:tracking-[-0.72]">
                 이
               </span>
@@ -43,11 +45,6 @@ const HomeHeader = ({ isLoggedIn, userName }: HomeHeaderProps) => {
 
       <p className="text-lg font-normal leading-[1.4] tracking-[-0.36px] text-black hidden md:block md:text-[28px] md:font-normal md:leading-[2.0] md:tracking-[-0.56]">
         안녕하세요. 건국대학교 학사정보시스템입니다.
-      </p>
-      <p className="text-xs mt-4 font-normal leading-[1.4] tracking-[-0.36px] text-danger md:text-[16px] md:font-normal md:leading-[2.0] md:tracking-[-0.56]">
-         본 화면의 학사정보는 예시용 가상 데이터입니다.
-         <br />
-         실제 학사 정보와 무관합니다.
       </p>
     </>
   );
